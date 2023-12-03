356a67d00ff459a0bb594dc6360a71a0a694b61e
23navi <navisureka23@gmail.com>
2023-12-03 15:09:14 +0530

    some middlewares

--- YoutubeClone - Full/server/src/utils/database.ts
+++ YoutubeClone - Full/server/src/utils/database.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import logger from "./logger";
 
 const DB_CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/youtube-clone";
+  process.env.DB_CONNECTION_STRING ||
+  "mongodb+srv://Navi2308:Navi2308@cluster0.nxyil8p.mongodb.net/?retryWrites=true&w=majority";
 
 export async function connectToDb() {
   try {
@@ -14,6 +15,17 @@ export async function connectToDb() {
   }
 }
 
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from DB");
+});
+
+mongoose.connection.on("reconnected", () => {
+  console.log("Reconnected after failure");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log({ err });
+});
 export async function disconnectFromDb() {
   await mongoose.connection.close();
   logger.info("Disconnect from db");
