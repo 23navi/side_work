3170f770cec8bbb0617e65bd896afc19f779f838
23navi <navisureka23@gmail.com>
2024-01-27 20:52:33 +0530

    .gitignore

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -6,6 +6,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
+import clientRoute from "./routes/client.js";
+import managementRoute from "./routes/management.js";
+import salesRoute from "./routes/sales.js";
+import generalRoute from "./routes/general.js";
+
 /* middleware configurations*/
 dotenv.cofigure();
 const app = express();
@@ -15,10 +20,10 @@ app.use(morgan("common"));
 app.use(helmet.corsOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 /*Routes setup*/
-app.use("/client",clientRoute);
-app.use("/management",managementRoute);
-app.use("/sales",salesRoute);
-app.use("/general",generalRoute);
+app.use("/client", clientRoute);
+app.use("/management", managementRoute);
+app.use("/sales", salesRoute);
+app.use("/general", generalRoute);
