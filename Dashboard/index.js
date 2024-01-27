d5276834acbaf28e475d92d7bb915fd9725b4c50
23navi <navisureka23@gmail.com>
2024-01-27 20:08:27 +0530

    init dashboard

---
+++ Dashboard/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+/* middleware configurations*/
+dotenv.cofigure();
