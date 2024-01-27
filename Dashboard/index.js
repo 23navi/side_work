b61267233b21d5c41a277f412a291f0d73e18286
23navi <navisureka23@gmail.com>
2024-01-27 20:15:38 +0530

    setup

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -8,3 +8,17 @@ import mongoose from "mongoose";
 
 /* middleware configurations*/
 dotenv.cofigure();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(helmet.corsOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+
+/*Routes setup*/
+app.use("/client",clientRoute);
+app.use("/management",managementRoute);
+app.use("/sales",salesRoute);
+app.use("/general",generalRoute);
