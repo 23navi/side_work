9be2adfdb3d2c6170030724d6fdcf4c49382a5ca
23navi <navisureka23@gmail.com>
2024-04-08 01:15:00 +0530

    bas

--- rabbitmq/basic/publisher.js
+++ rabbitmq/basic/publisher.js
@@ -3,9 +3,10 @@ const amqp = require("amqplib");
 async function connect() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel()
   } catch (e) {
     console.log(e);
   }
-}
+} 
 
 connect();
